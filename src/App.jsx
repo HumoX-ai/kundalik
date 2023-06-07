2e0d32a192a603cf2cfebed23c2cdaaf31e8b6b8
Humoyun2005 <abduodirovhumoyun@gmail.com>
2023-06-07 17:05:02 +0500

    anywhere

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(
+    localStorage.getItem("isLogin") === "true"
+  );
 
   const handleLogin = () => {
     setIsLogin(true);
+    localStorage.setItem("isLogin", true);
+  };
+  const handleLogout = () => {
+    setIsLogin(false);
+    localStorage.removeItem("isLogin");
   };
 
   return (
@@ -32,8 +39,14 @@ function App() {
 
         {isLogin ? (
           <>
-            <Route path="/student" element={<StudentPage />} />
-            <Route path="/teacher" element={<TeacherPage />} />
+            <Route
+              path="/student"
+              element={<StudentPage handleLogout={handleLogout} />}
+            />
+            <Route
+              path="/teacher"
+              element={<TeacherPage handleLogout={handleLogout} />}
+            />
           </>
         ) : (
           <Route path="*" element={<Navigate to="/404" />} />
--- src/components/StudentPage/StudentPage.jsx
+++ src/components/StudentPage/StudentPage.jsx
@@ -1,55 +1,55 @@
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import { BsChevronDown } from 'react-icons/bs'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
-
+/* eslint-disable react/prop-types */
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { BsChevronDown } from "react-icons/bs";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(() => ({
   backgroundColor: "#fff",
-  padding: "10px 10px"
-}))
-
+  padding: "10px 10px",
+}));
 
 function createData(full_name, ball) {
   return { full_name, ball };
 }
 
 const rows = [
-  createData('Laylo', 59),
-  createData('Oqila', 37),
-  createData('Nurillo', 62),
-  createData('Jamila', 35),
-  createData('Fozil', 56),
+  createData("Laylo", 59),
+  createData("Oqila", 37),
+  createData("Nurillo", 62),
+  createData("Jamila", 35),
+  createData("Fozil", 56),
 ];
 
-export const StudentPage = () => {
+export const StudentPage = ({ handleLogout }) => {
   return (
     <Box sx={{ flexGrow: 1, padding: 2 }}>
-      <Grid container spacing={{ xs: 2, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 4 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
         {Array.from(Array(6)).map((_, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={4}
-            md={4}
-            key={index}>
+          <Grid item xs={12} sm={4} md={4} key={index.name}>
             <Item>
               <Accordion
                 sx={{
                   backgroundColor: "#fff",
-                  boxShadow: "none"
-                }}>
+                  boxShadow: "none",
+                }}
+              >
                 <AccordionSummary
                   expandIcon={<BsChevronDown />}
                   aria-controls="panel1a-content"
@@ -58,9 +58,7 @@ export const StudentPage = () => {
                   <Typography>Group {index + 1}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                  <Table
-                    sx={{ minWidth: "full" }}
-                    aria-label="simple table">
+                  <Table sx={{ minWidth: "full" }} aria-label="simple table">
                     <TableHead>
                       <TableRow>
                         <TableCell>Name</TableCell>
@@ -72,24 +70,27 @@ export const StudentPage = () => {
                       {rows.map((row) => (
                         <TableRow
                           key={row.name}
-                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
                         >
-                          <TableCell
-                            component="th"
-                            scope="row">
+                          <TableCell component="th" scope="row">
                             {row.full_name}
                           </TableCell>
-                          <TableCell align="center">
-                            {row.ball}
-                          </TableCell>
+                          <TableCell align="center">{row.ball}</TableCell>
                           <TableCell align="right">
-                            <Button variant="contained" sx={{
-                              color: '#fff',
-                              backgroundColor: "#f00",
-                              "&:hover": {
-                                backgroundColor: '#f009'
-                              }
-                            }}>Kick</Button>
+                            <Button
+                              variant="contained"
+                              sx={{
+                                color: "#fff",
+                                backgroundColor: "#f00",
+                                "&:hover": {
+                                  backgroundColor: "#f009",
+                                },
+                              }}
+                            >
+                              Kick
+                            </Button>
                           </TableCell>
                         </TableRow>
                       ))}
@@ -97,11 +98,13 @@ export const StudentPage = () => {
                   </Table>
                 </AccordionDetails>
               </Accordion>
-
             </Item>
           </Grid>
         ))}
       </Grid>
-    </Box >
-  )
-}
+      <Button variant="contained" color="success" onClick={handleLogout}>
+        <Link to="/"> Chiqish</Link>
+      </Button>
+    </Box>
+  );
+};
